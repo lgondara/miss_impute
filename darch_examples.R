8f84aa1c0d66b44fcb36ca7e1b98a447f9d6761a
lgondara <lvdp00@gmail.com>
2016-04-27 09:30:55 -0400

    missforest added

--- darch_examples.R
+++ darch_examples.R
@@ -32,6 +32,7 @@ example.regression <- function(...)
 }
 example.regression()
 
+##cat dataset
 
 darch <- darch(Hwt ~ Bwt,
                cats[1:100,],
@@ -62,3 +63,85 @@ colnames(cat.test)=c("bwt")
 model.bm=bartMachine(cat.train,cats[1:100,3])
 predict.bm=predict(model.bm, cat.test)
 sqrt(mean((predict.bm-cats[101:144,3])^2))
+
+
+##glass dataset
+data("Glass")
+glass_shuffle=Glass[sample(nrow(Glass)),]
+darch <- darch(Type ~ .,
+               glass_shuffle[1:150,],
+               layers = c(0,100,50,30,20,10,7),
+               darch.batchSize =  10,
+               bp.learnRate = .01,
+               darch.isClass = T,
+               darch.numEpochs = 100,
+               darch.fineTuneFunction = "backpropagation",
+               darch.unitFunction = softmaxUnit)
+
+
+pred=predict(darch,newdata=glass_shuffle[151:214,], type="class")
+postResample(pred,glass_shuffle[151:214,10])
+
+model.rf <- randomForest(Type ~ .,data=glass_shuffle[1:150,])
+model.rf.pred=predict(model.rf,glass_shuffle[151:214,])
+postResample(model.rf.pred,glass_shuffle[151:214,10])
+
+
+##MNIST try
+# MNIST example with pre-training
+example.mnist <- function(dataFolder = "data/", downloadMNIST = T, ...)
+{
+  # Make sure to prove the correct folder if you have already downloaded the
+  # MNIST data somewhere, or otherwise set downloadMNIST to TRUE
+  provideMNIST(dataFolder, downloadMNIST)
+  
+  # Load MNIST data
+  load(paste0(dataFolder, "train.RData")) # trainData, trainLabels
+  load(paste0(dataFolder, "test.RData")) # testData, testLabels
+  
+  # only take 1000 samples, otherwise training takes increasingly long
+  chosenRowsTrain <- sample(1:nrow(trainData), size=1000)
+  trainDataSmall <- trainData[chosenRowsTrain,]
+  trainLabelsSmall <- trainLabels[chosenRowsTrain,]
+  
+  darch  <- darch(trainDataSmall, trainLabelsSmall,
+                  rbm.numEpochs = 5,
+                  rbm.consecutive = F, # each RBM is trained one epoch at a time
+                  rbm.batchSize = 100,
+                  rbm.lastLayer = -1, # don't train output layer
+                  rbm.allData = T, # use bootstrap validation data as well for training
+                  rbm.errorFunction = rmseError,
+                  rbm.initialMomentum = .5,
+                  rbm.finalMomentum = .7,
+                  rbm.learnRate = .1,
+                  rbm.learnRateScale = .98,
+                  rbm.momentumRampLength = .8,
+                  rbm.numCD = 2,
+                  rbm.unitFunction = sigmoidUnitRbm,
+                  rbm.weightDecay = .001,
+                  layers = c(784,100,10),
+                  darch.batchSize = 100,
+                  darch.dither = T,
+                  darch.initialMomentum = .4,
+                  darch.finalMomentum = .9,
+                  darch.momentumRampLength = .75,
+                  bp.learnRate = 1,
+                  bp.learnRateScale = .99,
+                  darch.unitFunction = c(tanhUnit, softmaxUnit),
+                  bootstrap = T,
+                  darch.numEpochs = 20,
+                  gputools = T, # try to use gputools
+                  gputools.deviceId = 0,
+                  ...
+  )
+  
+  predictions <- predict(darch, newdata=testData, type="class")
+  
+  labels <- cbind(predictions, testLabels)
+  numIncorrect <- sum(apply(labels, 1, function(i) { any(i[1:10] != i[11:20]) }))
+  cat(paste0("Incorrect classifications on test data: ", numIncorrect,
+             " (", round(numIncorrect/nrow(testLabels)*100, 2), "%)\n"))
+  
+  darch
+}
+example.mnist()
